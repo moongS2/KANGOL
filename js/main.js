8a16c11cb4979b7d5269fe6a3dc4cd51c8ba0b6d
moongmung <121421392+moongmung@users.noreply.github.com>
2023-01-31 18:02:16 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,207 +1,163 @@
-$(function(){
-    // 메인슬라이드
-    var swiper = new Swiper(".mySwiper", {
-        loop:true,
-        autoplay: {
-          delay: 4000,
-          disableOnInteraction: false,
-        },
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-      });
+$(document).ready(function(){        
+    //section.s3 의 갤러리
+    $('.buttons ul li:first').addClass('active');
+    $('.photos ul li').hide();
+    $('.photos ul li:first').show();
+    $('.buttons ul li').click(function(e){
+        e.preventDefault();
+            $('.buttons ul li').removeClass('active');
+            $(this).addClass('active');
+        var btn=$(this).index();
+        $('.photos ul li').each(function(){
+            if($(this).index()==btn){
+                $('.photos ul li').hide();
+                $(this).fadeIn();                
+            }
+        });       
+    });   
+   
+    $('.search_btn a').click(function(e){
+        e.preventDefault();
+        $.fn.fullpage.setAllowScrolling(false);
+        var str=$(this).parents('li').index()+1;
+        console.log(str);
+        $('<div />', { id: 'popup' }).appendTo('body'); 
+        $('<div />', { class: 'popup_box' }).appendTo('#popup');  
+        $('.popup_box').append('<img src="img/banner_'+str+'.png">');
+        $('#popup').append('<div class="close">X</div>');
+    });
+   
 
-
-  // 탭메뉴 슬라이드
-  var swiper2 = new Swiper(".new_product", {
-      loop:true,
-      pagination: {
-        el: ".tab_con1 .swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".tab_con1 .swiper-button-next",
-        prevEl: ".tab_con1 .swiper-button-prev",
-      },
-      breakpoints: {
-        
-        480: {
-          slidesPerView: 1,
-          spaceBetween: 0,
-        },
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 50,
-        },
-        1281: {
-          slidesPerView: 4,
-          spaceBetween: 20,
-        },
-      },
+    $('body').on('click', '#popup .close', function(){
+       $(this).parent().remove();
+       $.fn.fullpage.setAllowScrolling(true);
     });
 
-    var swiper3 = new Swiper(".best_product", {
-      loop:true,
-      pagination: {
-        el: ".tab_con2 .swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".tab_con2 .swiper-button-next",
-        prevEl: ".tab_con2 .swiper-button-prev",
-      },
-      breakpoints: {
-        
-        480: {
-          slidesPerView: 1,
-          spaceBetween: 0,
-        },
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 50,
-        },
-        1281: {
-          slidesPerView: 4,
-          spaceBetween: 20,
-        },
-      },
+    //웹포폴 탭메뉴
+    $('.con1').show();
+    $('.con2').hide();
+    $('.tt1').click(function(e){
+        e.preventDefault();
+        $('.con1').show();
+        $('.con2').hide();
+        $('.nemo1').show();
+        $('.nemo2').hide();
+    });
+    $('.tt2').click(function(e){
+        e.preventDefault();
+        $('.con1').hide();
+        $('.con2').show();
+        $('.nemo1').hide();
+        $('.nemo2').show();
     });
 
-    var swiper4 = new Swiper(".eventSlide", {
-      slidesPerView: 1.5,
-      spaceBetween: 30,
-      loop:true,
-      scrollbar: {
-        el: ".swiper-scrollbar",
-        hide: false,
-      },
-      navigation: {
-        nextEl: "section.event .swiper-button-next",
-        prevEl: "section.event .swiper-button-prev",
-      },
-      breakpoints: {
-        
-        480: {
-          slidesPerView: 1.5,
-          spaceBetween: 20,
-        },
-        768: {
-          slidesPerView: 2.5,
-          spaceBetween: 50,
-        },
-        1281: {
-          slidesPerView: 3,
-          spaceBetween: 80,
-        },
-      },
-    });
-
-
-
-  // 모든 a태그를 클릭하면 href속성 막기
-  $('a').click(function(e){
-    e.preventDefault();
-  });
-
-
-
-
-// 모바일에서만 실행
-if($(window).width() <= 1280) {
-
-    // 햄버거메뉴를 클릭하면 팝업메뉴가 왼쪽에서 나타남
-    $('.menu').click(function(){
-      $('.popup_menu').animate({'left':0});
-      // 윈도우의 화면 스크롤 막음
-      $('html,body').css('overflow-y','hidden');
-    });
-    // 팝업메뉴의 닫기버튼을 클릭하면 팝업 메뉴가 왼쪽으로 들어감
-    $('.close_btn').click(function(){
-      $('.popup_menu').animate({'left':'-110vw'});
-      // 윈도우의 화면 스크롤 활성화
-      $('html,body').css('overflow-y','visible');
-    });
-
-    // 아코디언 메뉴
-    var popup_nav = $('.popup_menu .popup_nav nav > ul > li > a');
-    popup_nav.click(function(){
-      // 만약 메뉴에 active가 설정되어 있지않으면
-      if($(this).attr('class') !='active'){
-          popup_nav.next().slideUp();
-          popup_nav.removeClass('active');
-          $(this).next().slideDown();
-          $(this).addClass('active');
-          
-
-      }else{
-          //다시 클릭한 주메뉴의 서브메뉴 들어감
-          $(this).next().slideUp();
-          //다시 클릭한 주메뉴에서 active클래스 제거
-          $(this).removeClass('active');
-
-      }
-    });
-  }else{
-    // 모바일이 아니면 실행
-    $('header nav .sub').hide();
-    $('header nav > ul > li > a').hover(function(){
-      if($(this).next().is('.sub')){
-        $('header nav .sub').stop().slideDown();
-        $('header nav .sub').css('z-index',11);
-        $(this).next().css('z-index',20);
-      }else{
-        $('header nav .sub').stop().slideUp();
-      }
-    });
-    $('header nav').mouseleave(function(){
-      $('header nav .sub').stop().slideUp();
-    });
+    //skill
+    $(document).ready(function(){
+        //fullpage
+        $('#fullpage').fullpage({
+            anchors:['HOME', 'ABOUT ME', 'DESIGN', 'PORTFOLIO', 'GUEST', 'CONTACT'],
+            navigation: true,
+            navigationPosition: 'right',
+            navigationTooltips: ['HOME', 'ABOUT ME', 'DESIGN', 'PORTFOLIO', 'GUEST', 'CONTACT'],
+            afterLoad:function(anchorLink,index){
+                if(index==1){
+                    $('.top').fadeOut();
+                }else{
+                    $('.top').fadeIn();
+                }
+            }
+        });
     
-    $('.top').click(function(){
-      //fullpage 플러그인을 사용할 때에는 명령어가 아래와 같습니다.
-      $.fn.fullpage.moveTo(1, 1);
-    });
-
     
-    $('#fullpage').fullpage({
-      afterLoad:function(anchorLink,index){
-        if(index==1){
-            $('.top').fadeOut();
-        }else{
-            $('.top').fadeIn();
+        var cnt1 = $("#count1");
+        var water1 = $("#water1");
+        var percent1 = 0;
+        var interval1=setInterval(skillWater1, 60);
+        function skillWater1(){
+            percent1++;
+            cnt1.text(percent1);  
+            water1.css({'transform':'translate(0'+','+(100-percent1)+'%'});
+            //percent값을 수정하기
+            if (percent1 == 80) {
+                clearInterval(interval1);
+            }
         }
-    }
-    }); 
-    
-
-  
-  }
-
-  
-  
-
-
-
-        // 탭메뉴
-    // 모든 탭내용 숨김
-    $('.tab_con > div').hide();
-    // 첫번째 탭내용만 보임
-    $('.tab_con > div:first').show();
-    $('.tab_title ul li:first').click(function(){
-      $('.tab_title ul li:first').addClass('active');
-      $('.tab_title ul li:last').removeClass('active');
-      $('.tab_con > div:first').show();
-      $('.tab_con > div:last').hide();
+        var cnt2 = $("#count2");
+        var water2 = $("#water2");
+        var percent2 = 0;
+        var interval2=setInterval(skillWater2, 60);
+        function skillWater2(){
+            percent2++;
+            cnt2.text(percent2);  
+            water2.css({'transform':'translate(0'+','+(100-percent2)+'%'});
+            //percent값을 수정하기
+            if (percent2 == 50) {
+                clearInterval(interval2);
+            }
+        }
+        var cnt3 = $("#count3");
+        var water3 = $("#water3");
+        var percent3 = 0;
+        var interval3=setInterval(skillWater3, 60);
+        function skillWater3(){
+            percent3++;
+            cnt3.text(percent3);  
+            water3.css({'transform':'translate(0'+','+(100-percent3)+'%'});
+            //percent값을 수정하기
+            if (percent3 == 80) {
+                clearInterval(interval3);
+            }
+        }
+        var cnt4 = $("#count4");
+        var water4 = $("#water4");
+        var percent4 = 0;
+        var interval4=setInterval(skillWater4, 60);
+        function skillWater4(){
+            percent4++;
+            cnt4.text(percent4);  
+            water4.css({'transform':'translate(0'+','+(100-percent4)+'%'});
+            //percent값을 수정하기
+            if (percent4 == 60) {
+                clearInterval(interval4);
+            }
+        }
+        var cnt5 = $("#count5");
+        var water5 = $("#water5");
+        var percent5 = 0;
+        var interval5=setInterval(skillWater5, 60);
+        function skillWater5(){
+            percent5++;
+            cnt5.text(percent5);  
+            water5.css({'transform':'translate(0'+','+(100-percent5)+'%'});
+            //percent값을 수정하기
+            if (percent5 == 50) {
+                clearInterval(interval5);
+            }
+        }
+        var cnt6 = $("#count6");
+        var water6 = $("#water6");
+        var percent6 = 0;
+        var interval6=setInterval(skillWater6, 60);
+        function skillWater6(){
+            percent6++;
+            cnt6.text(percent6);  
+            water6.css({'transform':'translate(0'+','+(100-percent6)+'%'});
+            //percent값을 수정하기
+            if (percent6 == 35) {
+                clearInterval(interval6);
+            }
+        }
     });
-    $('.tab_title ul li:last').click(function(){
-      $('.tab_title ul li:first').removeClass('active');
-      $('.tab_title ul li:last').addClass('active');
-      $('.tab_con > div:first').hide();
-      $('.tab_con > div:last').show();
-    });
-
-    
+    $('.top').click(function(){
+        //fullpage 플러그인을 사용할 때에는 명령어가 아래와 같습니다.
+        $.fn.fullpage.moveTo(1, 1);
+        
   
+      });
+  
+      
+
+  
+    
+    
 });
-
